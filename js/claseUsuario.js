32f8a05fc88bf9eaf9818d3b4e3689ec6039bb35
OscarQuintanilla <oscar.david.quintanilla@gmail.com>
2017-10-25 13:31:36 -0600

    Initial commit

---
+++ js/claseUsuario.js
@@ -0,0 +1,24 @@
+//Clase Usuario Reguistrado
+var UsuarioRegistrado = function(){
+	this.nombres;
+	this.apellidos;
+	this.correo;
+	this.contrasena;
+	this.dui;
+	this.nit;
+	this.celular;
+	this.fechas;
+	this.departamento;
+	this.municipio;
+	this.colonia;
+	this.calle;
+	this.casa;
+	this.pregunta;
+	this.respuesta;
+}
+var cuentaBanco = function(){
+	this.numeroDueno;
+	this.banco;
+	this.numeroCuenta;
+	this.saldoActual;
+}
